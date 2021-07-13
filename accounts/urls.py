b23873db53121bf1c6acd2a6c9dc563afba9603e
Gerald <geraldoduo@email.com>
2021-07-13 06:38:13 +0300

    removed assigned heroku domain from urls file

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('molymtech.herokuapp.com/',  views.IndexView.as_view(), name="index"),
-    path('molymtech.herokuapp.com/web-design', views.WebDesignView.as_view(), name='web-design'),
-    path('molymtech.herokuapp.com/graphics-design', views.GraphicsDesignView.as_view(), name='graphics-design'),
-    path('molymtech.herokuapp.com/software-development', views.SoftwareDevelopmentView.as_view(), name='software-development'),
-    path('molymtech.herokuapp.com/web-design/home', views.IndexView.as_view(), name="back-to-home"),
-    path('molymtech.herokuapp.com/web-design/services', views.IndexView.as_view(), name="back-to-home"),
-    path('molymtech.herokuapp.com/ContactForm', views.ContactFormView, name="ContactForm"),
+    path('index/',  views.IndexView.as_view(), name="index"),
+    path('index/web-design', views.WebDesignView.as_view(), name='web-design'),
+    path('index/graphics-design', views.GraphicsDesignView.as_view(), name='graphics-design'),
+    path('index/software-development', views.SoftwareDevelopmentView.as_view(), name='software-development'),
+    path('index/web-design/home', views.IndexView.as_view(), name="back-to-home"),
+    path('index/web-design/services', views.IndexView.as_view(), name="back-to-home"),
+    path('index/ContactForm', views.ContactFormView, name="ContactForm"),
     
     
 
